93625a441d3e5b5b5e68c21bbe88f1305237166e
Amos Haviv <mail@amoshaviv.com>
2016-05-31 18:54:39 +0200

    Setting Interval

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
+var counter = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('test').innerHTML = navigator.userAgent;
+
+	document.getElementById('useragent').innerHTML = navigator.userAgent;
+
+	window.setInterval(function() {
+		document.getElementById('interval').innerHTML = counter;
+		counter++;
+	}, 1);
 }, false);
