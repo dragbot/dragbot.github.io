3eb6778cfb07f550c4a3dda2fbc60738d611378d
Amos Haviv <mail@amoshaviv.com>
2016-06-01 10:28:03 +0200

    Adding interval time

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('useragent').innerHTML = navigator.userAgent;
 
     window.setInterval(function() {
-    	document.getElementById('interval').innerHTML = counter;
+    	// document.getElementById('interval').innerHTML = counter;
     	counter++;
     }, 100);
 
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setInterval(function() {
         var currentTime = new Date()
+        document.getElementById('interval').innerHTML = counter;
         document.getElementById('interval_time').innerHTML += (currentTime.toTimeString().split(' ')[0] + ' - ')
     }, 1000)
 }, false);
